34a6d0a6dd3f02bb90972ddd1023ba8746b25eac
Henri Sivonen <hsivonen@hsivonen.fi>
2018-03-11 19:34:29 +0200

    Make the app convert stuff.
    Copied the license header from recode_rs due to copying some bits from there.

--- recodeqmain.cpp
+++ recodeqmain.cpp
@@ -1,6 +1,13 @@
-#include "recodeqmain.h"
+// Copyright 2016 Mozilla Foundation. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
-#include <QTextStream>
+#include "recodeqmain.h"
 
 RecodeQMain::RecodeQMain(const QStringList& inputs,
                          const QString& output,
@@ -14,25 +21,101 @@ RecodeQMain::RecodeQMain(const QStringList& inputs,
 {
 }
 
+QTextCodec*
+RecodeQMain::codecForLabel(const QString& label)
+{
+    if (label.isEmpty()) {
+        return QTextCodec::codecForName("UTF-8");
+    }
+    return QTextCodec::codecForName(label.toUtf8());
+}
+
+void
+RecodeQMain::writeToStdErrAndQuit(const QString& message)
+{
+    QTextStream err(stderr);
+    err << message;
+    emit done();
+}
+
+void
+RecodeQMain::convert(QFile& inputFile, QTextCodec* inputCodec, QTextStream& outputStream)
+{
+    QTextStream inputStream(&inputFile);
+    inputStream.setCodec(inputCodec);
+
+    // No signaling of io errors???
+    QString text = inputStream.readAll();
+    outputStream << text;
+}
+
 void
 RecodeQMain::run()
 {
-    QTextStream out(stdout);
-    out << "Output: ";
-    out << output;
-    out << "\n";
-    out << "From: ";
-    out << fromLabel;
-    out << "\n";
-    out << "To: ";
-    out << toLabel;
-    out << "\n";
-    out << "Positional: ";
-    for (auto&& arg: inputs) {
-        out << "\n";
-        out << arg;
+    QTextCodec* inputCodec = codecForLabel(fromLabel);
+    if (!inputCodec) {
+        writeToStdErrAndQuit(fromLabel + " is not a known encoding label; exiting.");
+        return;
     }
-    out << "\n";
 
+    QTextCodec* outputCodec = codecForLabel(toLabel);
+    if (!outputCodec) {
+        writeToStdErrAndQuit(toLabel + " is not a known encoding label; exiting.");
+        return;
+    }
+
+    QFile outputFile;
+    if (output.isEmpty() || output == "-") {
+        if (!outputFile.open(stdout, QIODevice::WriteOnly)) {
+            writeToStdErrAndQuit("Cannot open stdout for writing; exiting.");
+            return;
+        }
+    } else {
+        outputFile.setFileName(output);
+        if (!outputFile.open(QIODevice::WriteOnly)){
+            writeToStdErrAndQuit(QString("Cannot open ") + output + "for writing; exiting.");
+            return;
+        }
+    }
+    QTextStream outputStream(&outputFile);
+    outputStream.setCodec(outputCodec);
+
+    if (inputs.isEmpty()) {
+        QFile inputFile;
+        if (!inputFile.open(stdin, QIODevice::ReadOnly)) {
+            writeToStdErrAndQuit("Cannot open stdin for writing; exiting.");
+            return;
+        }
+        convert(inputFile, inputCodec, outputStream);
+        inputFile.close();
+    } else {
+        QString fileName;
+        foreach (fileName, inputs) {
+            if (fileName.isEmpty()) {
+                // shouldn't happen with shell
+            } else if (fileName == "-") {
+                QFile inputFile;
+                if (!inputFile.open(stdin, QIODevice::ReadOnly)) {
+                    writeToStdErrAndQuit("Cannot open stdin for writing; exiting.");
+                    return;
+                }
+                convert(inputFile, inputCodec, outputStream);
+                inputFile.close();
+            } else {
+                QFile inputFile;
+                inputFile.setFileName(fileName);
+                if (!inputFile.open(QIODevice::ReadOnly)){
+                    writeToStdErrAndQuit(QString("Cannot open ") + fileName + "for reading; exiting.");
+                    return;
+                }
+                convert(inputFile, inputCodec, outputStream);
+                inputFile.close();
+            }
+        }
+    }
+
+    // Move these into a RAII thingy
+    outputStream.flush();
+    outputFile.close();
     emit done();
 }
--- recodeqmain.h
+++ recodeqmain.h
@@ -1,7 +1,19 @@
+// Copyright 2016 Mozilla Foundation. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #ifndef RECODEQMAIN_H
 #define RECODEQMAIN_H
 
 #include <QObject>
+#include <QTextCodec>
+#include <QTextStream>
+#include <QFile>
 
 class RecodeQMain : public QObject
 {
@@ -19,6 +31,10 @@ public slots:
     void run();
 
 private:
+    QTextCodec* codecForLabel(const QString& label);
+    void writeToStdErrAndQuit(const QString& message);
+    void convert(QFile& inputFile, QTextCodec* inputCodec, QTextStream& outputStream);
+
     QStringList inputs;
     const QString output;
     const QString fromLabel;
